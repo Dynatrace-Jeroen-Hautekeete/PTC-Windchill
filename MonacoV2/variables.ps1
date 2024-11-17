3d6a3b580f1ded69d231439699ff1c5f2ab06d07
Dynatrace-Jeroen-Hautekeete <jeroen.hautekeete@dynatrace.com>
2024-11-17 13:07:58 +0100

    Rename and copy script examples

--- MonacoV2/variables.ps1
+++ MonacoV2/variables.ps1
@@ -3,4 +3,3 @@
 # tenant
 $env:tenant_token="dt0c01.XXX.YYYYYY"
 $env:tenant_private_location="SYNTHETIC_LOCATION-ABCDEFGHIJKLMNOP"
-
---
+++ MonacoV2/variables.sh
@@ -0,0 +1,5 @@
+# environment variables for Monaco
+
+# tenant
+export tenant_token="dt0c01.XXX.YYYYYY"
+export tenant_private_location="SYNTHETIC_LOCATION-ABCDEFGHIJKLMNOP"
